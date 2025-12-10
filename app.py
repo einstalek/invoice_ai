7b0e267611c7de3e77d386c3b12f8f445694d5f8
einstalek <arganaidi@phystech.edu>
2025-12-09 23:53:28 -0800

    deployemnt with railway

--- app.py
+++ app.py
@@ -60,4 +60,7 @@ def process_file():
         shutil.rmtree(temp_dir)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    # app.run(debug=True, port=8000)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port, debug=False)
+
